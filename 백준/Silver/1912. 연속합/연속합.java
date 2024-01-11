7c08440116636602409ac73632a04a54c600afd9
KIM INBEOM <63138047+Inbeom-970210@users.noreply.github.com>
2024-01-12 01:15:58 +0900

    [Silver II] Title: 연속합, Time: 204 ms, Memory: 22560 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,24 +1,44 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();          // 정수의 개수
-        int[] dp = new int[n];         // dp
-        int[] arr = new int[n];        // 주어진 정수가 입력된 배열
-        
-        for(int i = 0; i < n; i++) {    // 정수 입력, 가장 큰 수 구하기
-            arr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // n 은 정수 개수; 1 <= n <= 100,000
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int res = Integer.parseInt(st.nextToken());
+        int[] sum = {0, 0};
+        if(res > 0) {
+            sum[0] = res;
+            sum[1]++;
         }
-        
-        int res = arr[0];              // 가장 큰 합
-        dp[0] = arr[0];
-        
-        for(int idx = 1; idx < n; idx++) {
-            dp[idx] = Math.max(dp[idx - 1] + arr[idx], arr[idx]);
-            res = Math.max(res, dp[idx]);
+        for(int i = 2; i <= n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num >= 0) {
+                sum[0] += num;
+                sum[1] ++;
+            }
+            else if(num + sum[0] <= 0) {
+                if(sum[1] < 1) res = Math.max(res, num);
+                else res = Math.max(res, sum[0]);
+                sum[0] = 0;
+                sum[1] = 0;
+            }
+            else {
+                res = Math.max(res, sum[0]);
+                sum[0] += num;
+                sum[1] ++;
+            }
+//            System.out.println("num: " + num);
+//            System.out.println(i + ": " + res + "// sum: " + sum[0]);
         }
-        
+
+        if(sum[1] > 0) res = Math.max(res, sum[0]);
         System.out.print(res);
     }
 }

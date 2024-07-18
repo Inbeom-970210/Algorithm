f7fe826c6543d205fb3cdaca4e085801db9a39c6
KIM INBEOM <63138047+Inbeom-970210@users.noreply.github.com>
2024-07-19 05:24:42 +0900

    [Gold IV] Title: 목장 건설하기, Time: 400 ms, Memory: 73692 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
@@ -4,45 +4,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, answer;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        int[][] map = new int[M][N];
-        for (int r = 0; r < M; r++) {
-            st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < N; c++) {
-                map[r][c] = Integer.parseInt(st.nextToken());
-            }
-        }
 
-        answer = 0;
-        for (int r = 0; r < M; r++) {
-            for (int c = 0; c < N; c++) {
-                search(r, c, map);
+        // 풀이: 점화식을 사용한 DP
+        // 1. 좌표를 정사각형의 우측 하단 모서리로 인식
+        // 2. DP는 해당 좌표에서 만들 수 있는 가장 큰 정사각형 목장
+        // 3. 좌표 기준 대각선, 좌, 상의 최소 대각선 + 1이 해당 좌표의 값
+        int answer = 0;
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] DP = new int[M + 1][N + 1];
+
+        for (int r = 1; r <= M; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 1; c <= N; c++) {
+                if (Integer.parseInt(st.nextToken()) > 0) continue;
+
+                DP[r][c] = Math.min(DP[r - 1][c - 1], Math.min(DP[r - 1][c], DP[r][c - 1])) + 1;
+                answer = Math.max(answer, DP[r][c]);
             }
         }
 
         System.out.print(answer);
     }
-
-    private static void search(int row, int col, int[][] map) {
-        int len = answer;
-        loof:
-        while (row + len < M && col + len < N) {
-            for (int r = row; r <= row + len; r++) {
-                for (int c = col; c <= col + len; c++) {
-                    if (map[r][c] > 0) {
-                        break loof;
-                    }
-                }
-            }
-            len++;
-        }
-        answer = len;
-    }
-
 }

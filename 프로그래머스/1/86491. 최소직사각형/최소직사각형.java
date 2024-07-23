a9b989d35724a4ea9d97e2c5fe7ab84434975125
KIM INBEOM <63138047+Inbeom-970210@users.noreply.github.com>
2024-07-23 14:43:22 +0900

    [level 1] Title: 최소직사각형, Time: 2.06 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int w = 0;
+        int h = 0;
+        for(int[] size : sizes) {
+            int max = Math.max(size[0], size[1]);
+            int min = Math.min(size[0], size[1]);
+            w = Math.max(w, max);
+            h = Math.max(h, min);
+        }
+        
+        return w * h;
+    }
+}

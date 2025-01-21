35235c92790bb9a7845f9552da22332384f00e9a
KIM INBEOM <63138047+Inbeom-970210@users.noreply.github.com>
2025-01-21 23:53:29 +0900

    [Silver V] Title: 올림픽, Time: 192 ms, Memory: 17764 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        char[][] countries = new char[N][4];
+        for (int i = 0; i < N; i++) {
+            countries[i] = br.readLine().toCharArray();
+        }
+
+        Arrays.sort(countries, (o1, o2) -> {
+            if (o1[1] > o2[1]) return -1;
+            else if (o1[1] < o2[1]) return 1;
+            else {
+                if (o1[2] > o2[2]) return -1;
+                else if (o1[2] < o2[2]) return 1;
+                else {
+                    if (o1[3] >= o2[3]) return -1;
+                    else return 1;
+                }
+            }
+        });
+
+        int answer = 0;
+        loop:
+        for (int i = 0; i < N; i++) {
+            if (K == countries[i][0] - '0') {
+                while (i > 0) {
+                    answer = i + 1;
+                    for (int j = 1; j < 4; j++) {
+                        if (countries[i - 1][j] > countries[i][j]) break loop;
+                    }
+                    i--;
+                }
+
+                answer = 1;
+                break;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}

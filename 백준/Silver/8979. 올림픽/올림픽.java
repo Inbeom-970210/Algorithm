6a83ab5263f75dff4309ab0ffdb1da8ccbd5c118
KIM INBEOM <63138047+Inbeom-970210@users.noreply.github.com>
2025-01-22 00:23:24 +0900

    [Silver V] Title: 올림픽, Time: 196 ms, Memory: 18148 KB, Score: 28 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -12,9 +12,12 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        char[][] countries = new char[N][4];
+        int[][] countries = new int[N][4];
         for (int i = 0; i < N; i++) {
-            countries[i] = br.readLine().toCharArray();
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                countries[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
         Arrays.sort(countries, (o1, o2) -> {
@@ -24,7 +27,7 @@ public class Main {
                 if (o1[2] > o2[2]) return -1;
                 else if (o1[2] < o2[2]) return 1;
                 else {
-                    if (o1[3] >= o2[3]) return -1;
+                    if (o1[3] < o2[3]) return -1;
                     else return 1;
                 }
             }
@@ -33,7 +36,7 @@ public class Main {
         int answer = 0;
         loop:
         for (int i = 0; i < N; i++) {
-            if (K == countries[i][0] - '0') {
+            if (K == countries[i][0]) {
                 while (i > 0) {
                     answer = i + 1;
                     for (int j = 1; j < 4; j++) {
